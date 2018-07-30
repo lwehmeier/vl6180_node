adf928486f5b359d3058293c7f5c6cbd895a2c6b
Leon Wehmeier <lw96@st-andrews.ac.uk>
2018-07-30 12:36:01 +0100

    fixed hw init and driver calls

--- vl6180.py
+++ vl6180.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 import math
 from math import sin, cos, pi, radians
 import rospy
@@ -8,7 +8,7 @@ import struct
 from tca9548_driver.tca9548 import I2C_SW
 from vl6180_driver.ST_VL6180X import VL6180X
 import numpy as np
-
+import time
 VL_CHANNELS = {"lf" : 0x03, "lr" : 0x02, "rf": 0x04, "rr": 0x05, "fl": 0x06, "fr": 0x07}
 DISTANCE_THRESHOLD =  200
 
@@ -17,20 +17,29 @@ class VL6180:
         self.distance = 255
         self.res = 0.1
         self.angle = angle
-        tca.chn(VL_CHANNELS(angle))
+        tca.chn(VL_CHANNELS[angle])
+        time.sleep(0.1)
         self.vl = VL6180X()
-        self.vl.get_distance()
+        self.vl.get_identification()
+        print(self.vl.idModel)
+        #if not self.vl.ready:
+        #    raise Exception("VL "+str(angle)+" failed to initialise")
+        #self.vl.default_settings()
+        #self.vl.get_distance()
     def update(self):
         angle = self.angle
         ch = VL_CHANNELS[angle]
         tca.chn(ch)
+        time.sleep(0.1)
         self.distance = self.vl.get_distance()
+        #print(self.distance)
         if self.distance < DISTANCE_THRESHOLD:
             self.interrupt()
     def interrupt(self):
         print("VL at "+ str(self.angle) +" detected obstacle. Stopping platform..")
         e_stop.publish(Int16(1))
 
+tca = I2C_SW('I2C switch 0',0X70, 1)
 vl_array = [
     VL6180("lf"),
     VL6180("lr"),
@@ -52,5 +61,5 @@ cmap=None
 rospy.init_node('vl6180')
 e_stop = rospy.Publisher("/platform/e_stop", Int16, queue_size=1, tcp_nodelay=True)
 rospy.Timer(rospy.Duration(0.5), callbackTimer)
-r = rospy.Rate(2)
+r = rospy.Rate(0.5)
 rospy.spin()
